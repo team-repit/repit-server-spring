2ad54b832a86ba72ad45cdc0ef7cbdfb42d10cc1
astrokan <astrokan@naver.com>
2025-06-22 23:18:59 +0900

    refactor: 배포 오류 수정 목적의 DB bean 비활성화

--- src/test/java/com/repit/api/ApiApplicationTests.java
+++ src/test/java/com/repit/api/ApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.repit.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class ApiApplicationTests {
 
 	@Test
