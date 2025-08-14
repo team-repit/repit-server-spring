895d8ac3d87719d1a4296c960540b0d386930a26
PADO <sanhobox@gmail.com>
2025-08-14 21:18:13 +0900

    feat(webrtc) : signaling server 구축

--- src/main/java/com/repit/repository/MemberRepository.java
+++ src/main/java/com/repit/repository/MemberRepository.java
@@ -1,6 +1,6 @@
 package com.repit.repository;
 
-import com.example.yourpackage.domain.Member;
+import com.repit.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/repit/signaling/WebSocketConfig.java
+++ src/main/java/com/repit/signaling/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.repit.signaling;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
 // 시그널링 서버 구축
@@ -12,11 +11,11 @@ import org.springframework.web.socket.config.annotation.*;
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final SignalHandler signalHandler;
+    private final com.example.webrtc.SignalHandler signalHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalHandler, "/signal")
-                .setAllowedOrigins("*");
+        registry.addHandler(signalHandler, "/signal") // "/signal" 경로에 핸들러 등록
+                .setAllowedOrigins("*"); // 모든 출처에서 오는 요청을 허용 (CORS)
     }
 }
