cf94c54fb941783b744c251adf10ed6587747302
astrokan <astrokan@naver.com>
2025-10-03 15:20:02 +0900

    [docs] 테스트 환경 설정 개선: H2 인메모리 DB 사용으로 테스트 실패 해결

--- src/test/java/com/repit/api/ApiApplicationTests.java
+++ src/test/java/com/repit/api/ApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.repit.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
+@SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
 
 	@Test
